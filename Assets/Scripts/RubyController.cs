622f2e7ef5355ef2750faace626cfaf2431a7fbc
Stephen Craton <scraton@users.noreply.github.com>
2020-02-22 22:06:02 -0600

    Update RubyController.cs
    This might make it reverse direction after it goes more than 320 pixels to the right?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    private int direction;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + (0.1f * direction);
+        
+        if (position.x > 320) {
+            direction = direction * -1;
+        }
+        
         transform.position = position;
     }
 }
